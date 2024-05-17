e368e4789a068da189e4cfc2ba22fab9adb7dfb8
gabrieljoseribeiroporto <gabrieljoseribeiroporto@gmail.com>
2024-05-16 22:57:19 -0300

    criptografia

--- Strings/32_manipulacao_de_strings.php
+++ Strings/32_manipulacao_de_strings.php
@@ -1,6 +1,18 @@
 <?php
 
-$salt = 'asalOOHSAISsakAUYSGISB@#!@$@#DAdawd';
-$password = '123456';
+$password = '123456444';
 
-echo md5($password);
+//echo md5($password);
+//echo sha1($password);
+//echo crypt($password, $salt);
+$passCr = base64_encode($password);
+//echo $passCr;
+//echo base64_decode($passCr);
+//echo hash('sha512', $password);
+
+echo cryptC($password);
+
+function cryptC(String $value): String
+{
+    return hash('sha512', sha1(md5(crypt($value, 'asalOOHSAAUYSGISB@#!@$@#DAdawd'))));
+}
